08cdba222f56246a36af7c0d28d37e5286e7697b
chinesebear <swubear@163.com>
2022-07-17 17:39:35 +0800

    init paper_analyzer

---
+++ src/json2csv_conference.py
@@ -0,0 +1,42 @@
+import json
+import csv
+from traceback import print_list
+
+
+# Opening JSON file and loading the data
+# into the variable data
+with open('info.json') as json_file:
+    data = json.load(json_file)
+
+employee_data = data['result']['hits']['hit']
+
+# now we will open a file for writing
+data_file = open('info_conference.csv', 'w')
+
+# create the csv writer object
+csv_writer = csv.writer(data_file)
+
+# Counter variable used for writing
+# headers to the CSV file
+count = 0
+
+for emp in employee_data:
+    rec = emp['info']
+    if rec['type'] == 'Conference and Workshop Papers':
+        if count == 0:
+            # Writing headers of CSV file
+            header = rec.keys()
+            csv_writer.writerow(header)
+            count += 1
+        # Writing data of CSV file
+        authors = ""
+        #print(type(rec['authors']['author']))
+        if type(rec['authors']['author']).__name__ == 'dict':
+            authors = rec['authors']['author']['text']
+        if type(rec['authors']['author']).__name__ == 'list':
+            for au in rec['authors']['author']:
+                if type(au).__name__ == 'dict':
+                    authors += au['text'] + ';'
+        rec['authors'] = authors
+        csv_writer.writerow(rec.values())
+data_file.close()
